20972ddab5ad2314fd582a4b377b4a17c250e5ac
Justin Kozlowski <justinkozlowski@justins-air.mynetworksettings.com>
2025-05-03 12:24:01 -0400

    fix scroll

--- src/App.vue
+++ src/App.vue
@@ -25,11 +25,12 @@ export default {
       this.isKeyboardOpen = currentHeight < this.originalHeight - 150;
       document.documentElement.style.setProperty('--viewport-height', `${currentHeight}px`);
       if (this.isKeyboardOpen){
-        setTimeout(() => {
+        // setTimeout(() => {
+        //   window.scrollTo(0, 0);
+        // }, 2);
+        this.$nextTick(() => {
           window.scrollTo(0, 0);
-        }, 2);
-        // this.$nextTick(() => {
-        // })
+        });
       }
     },
   },
